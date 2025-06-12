f0af9eff351e1b6840dbfdcb35b2fbadecd481ad
Yannic Hillers <yannic.hillers@tuhh.de>
2025-06-12 14:20:12 +0200

    Reset

--- task_3-1/src/main.c
+++ task_3-1/src/main.c
@@ -1,45 +1,3 @@
-<<<<<<< HEAD
-#include "ses_button.h"
-#include "ses_led.h"
-#include <avr/io.h>
-#include <avr/interrupt.h>
-#include <util/delay.h>
-
-// Function to toggle the red LED
-void toggleRedLed(void) {
-    led_redToggle(); // Toggle the red LED
-}
-
-// Function to toggle the green LED
-void toggleGreenLed(void) {
-    led_greenToggle(); // Toggle the green LED
-}
-
-int main(void) {
-    // Initialize LEDs
-    led_redInit();
-    led_greenInit();
-    sei();
-
-    // Turn off LEDs initially
-    led_redOff();
-    led_greenOff();
-
-    // Initialize buttons
-    button_init();
-
-    // Set button callbacks
-    button_setRotaryButtonCallback(toggleRedLed);   // Rotary button toggles red LED
-    button_setPushButtonCallback(toggleGreenLed);  // Push button toggles green LED
-
-    // Main loop
-    while (1) {
-        
-        // The program relies on interrupts to handle button presses
-        // No additional code is needed in the main loop
-        _delay_ms(500);
-
-=======
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include "ses_button.c"
@@ -67,12 +25,7 @@ int main(void) {
 
     while(1){
         _delay_ms(100);
->>>>>>> Jannis
     }
 
     return 0;
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> Jannis
--- task_3-3/src/main.c
+++ task_3-3/src/main.c
@@ -1,39 +1,4 @@
-<<<<<<< HEAD
-#include "ses_button.h"
-#include "ses_led.h"
-#include <avr/io.h>
-#include <avr/interrupt.h>
-#include <util/delay.h>
-
-
-int main(void) {
-    // Initialize LEDs
-    led_redInit();
-    led_greenInit();
-    sei();
-
-    // Initialize buttons
-    button_init(true);
-
-    // Set button callbacks
-    button_setRotaryButtonCallback(led_redToggle);   // Rotary button toggles red LED
-    button_setPushButtonCallback(led_greenToggle);  // Push button toggles green LED
-
-    // Main loop
-    while (1) {
-        
-        // The program relies on interrupts to handle button presses
-        // No additional code is needed in the main loop
-        _delay_ms(500);
-
-    }
-
-    return 0;
-}
-
-=======
 
 int main(void) {
 
 }
->>>>>>> Jannis
